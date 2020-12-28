42e08a7ff7f4c7376769504fe9b2dd3171433ebe
Edmond Wu <edmond.wu@velocitylight.net>
2020-12-28 10:16:53 +0800

    Update

--- middleware/tokenVerifyMiddleware.js
+++ middleware/tokenVerifyMiddleware.js
@@ -4,7 +4,8 @@ const tokenVerifyMiddleware =  async (req, res, next) => {
   try {
     const authorization = req.headers.authorization;
     const token = authorization.split(' ')[1];
-    const user = await agServer.auth.verifyToken(token, agServer.signatureKey)
+    const user = await agServer.auth.verifyToken(token, agServer.signatureKey);
+    
     req.user = user;
     next();
   } catch(error) {
