520c8de6a084260ae3bba622f0ebebde1efe83eb
Edmond Wu <edmond.wu@velocitylight.net>
2020-12-29 19:20:35 +0800

    add header

--- router/authRoute.js
+++ router/authRoute.js
@@ -36,6 +36,10 @@ const login = async (req, res) => {
     const signInfo = pick(user, ["uuid", "account"]);
     const token = generateToken(signInfo);
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    
     return successResponse(res, {
       token,
       account: user.account,
